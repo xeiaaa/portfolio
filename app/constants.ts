729c8547d3ecfca2676315b3858f1abfd12f719d
Bret Axl Sebastian Pura <cosmicdevcs@gmail.com>
2025-08-21 06:59:28 +0800

    add article

--- app/constants.ts
+++ app/constants.ts
@@ -428,6 +428,14 @@ export const Profile = {
 
 export const articles = [
   {
+    title:
+      "Terminal Shortcut for macOS iTerm: Launch Your Dev Environment with One Command",
+    date: "2025",
+    link: "https://bretaxlsebastian.hashnode.dev/macos-iterm-dev-shortcut",
+    image:
+      "https://images.unsplash.com/photo-1590971862391-06cac0657603?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
     title: "Nike By You Clone - Timelapse",
     date: "2023",
     link: "https://youtu.be/ReLVAtBcQso",
